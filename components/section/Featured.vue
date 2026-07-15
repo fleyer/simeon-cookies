45291e1c9f21c43ff93b22ed9291ad1b8800b180
Romain Manchado <manchado.romain@gmail.com>
2026-07-15 17:44:52 +0200

    fix(featured section) : minor code style change

--- components/section/Featured.vue
+++ components/section/Featured.vue
@@ -4,24 +4,24 @@ import { UPageCard } from '#components';
 import type Card from '../product/Card.vue';
 
 const product = {
-    title: "Cookie à la fraise",
-    subtitle: "les meilleurs, ils vont vous faire fondre !",
-    image: "/products/cookies-strawberry.jpg",
-    imageAlt: "cookie à la fraise",
-    status: "featured",
-    category: "NOUVEAU !",
-    description: "Un mélange de couleur",
-    link: "/"
+  title: "Cookie à la fraise",
+  subtitle: "les meilleurs, ils vont vous faire fondre !",
+  image: "/products/cookies-strawberry.jpg",
+  imageAlt: "cookie à la fraise",
+  status: "featured",
+  category: "NOUVEAU !",
+  description: "Un mélange de couleur",
+  link: "/"
 } satisfies ComponentProps<typeof Card>
 
 const product2 = {
-    title: "Cookie au miel et au chocolat",
-    subtitle: "N'attendez pas goutez les",
-    image: "/products/cookies-honey-chocolate.jpg",
-    imageAlt: "cookie au miel et au chocolat",
-    category: "Les classiques",
-    description: "Vous ne pourrez plus vous arretez",
-    link: "/"
+  title: "Cookie au miel et au chocolat",
+  subtitle: "N'attendez pas goutez les",
+  image: "/products/cookies-honey-chocolate.jpg",
+  imageAlt: "cookie au miel et au chocolat",
+  category: "Les classiques",
+  description: "Vous ne pourrez plus vous arretez",
+  link: "/"
 } satisfies ComponentProps<typeof Card>
 
 </script>
